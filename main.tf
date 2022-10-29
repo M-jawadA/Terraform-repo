56c4726993b1fe3d355d55a3e0ece79df67dfdf1
m-jawadA <jawadah303@mail.com>
2022-10-29 21:57:15 +0500

    setup ec2 instance

--- main.tf
+++ main.tf
@@ -1,35 +1,155 @@
 provider "aws" {
-  region     = "ap-south-1"
+  region = "ap-south-1"
 }
 
+variable "vpc_cider_block" {
+  description = "vpc cider block"
+}
 variable "subnet_cider_block" {
   description = "subnet cider block"
 }
+variable "avail_zone" {}
+variable "env_prefix" {}
+variable "my_ip" {}
+variable "instance-type" { 
+}
+variable "public-key-location" {
+  
+}
 
-resource "aws_vpc" "developmen-vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cider_block
   tags = {
-    Name = "development"
+    Name = "${var.env_prefix}-vpc"
   }
 }
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id            = aws_vpc.developmen-vpc.id
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id            = aws_vpc.myapp-vpc.id
   cidr_block        = var.subnet_cider_block
-  availability_zone = "ap-south-1a"
+  availability_zone = var.avail_zone
   tags = {
-    Name = "development-subnet"
+    Name = "${var.env_prefix}-subnet-1"
   }
 
 }
+
+# for route to connect to internet we need igw
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+   tags = {
+     Name = "${var.env_prefix}-igw"
+   }
+}
+ #craeate own route table
+ /*resource "aws_route_table" "myapp-route-table" {
+   vpc_id = aws_vpc.myapp-vpc.id
+
+   route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+   }
+   tags = {
+     Name = "${var.env_prefix}-rtb"
+   }
+ }*/
+
+ # accociate subnets with route table
+ /*resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+   
+ }*/
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+   route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+   }
+  tags = {
+    Name="${var.env_prefix}-main-rtb"
+  }
+}
+
+# add security group
+resource "aws_default_security_group" "default-sg" {
+
+  vpc_id = aws_vpc.myapp-vpc.id
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [ var.my_ip ]
+  }
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+  tags = {
+    Name="${var.env_prefix}-default-sg"
+  }
+}
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = [ "amazon" ]
+  filter {
+    name = "name"
+    values = [ "amzn2-ami-kernel-*-x86_64-gp2" ]
+  }
+  filter {
+    name="virtualization-type"
+    values = [ "hvm" ]
+  }
+}
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+output "ec2_publik_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
+# generate key 
+resource "aws_key_pair" "ssh-key" {
+  key_name = "my-key"
+  public_key = file(var.public-key-location)
+}
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance-type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id] 
+  availability_zone = var.avail_zone
+  associate_public_ip_address = true
+  key_name =aws_key_pair.ssh-key.key_name
+  
+  user_data = file("start-script.sh")
+  tags = {
+    Name = "${var.env_prefix}-server-myapp"
+  }
+  
+}
+
+
+
 # data "aws_vpc" "existing-vpc" {
 #   default = true
 
 # }
 
-output "dev-vpc-id" {
-  value = aws_vpc.developmen-vpc.id
-}
+# output "dev-vpc-id" {
+#   value = aws_vpc.developmen-vpc.id
+# }
 
-output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
-}
+# output "dev-subnet-id" {
+#   value = aws_subnet.dev-subnet-1.id
+# }
